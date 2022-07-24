8f6031e4ca2daec219fe0b70b48fdfa5154095ee
William Jones <kc5uyn@gmail.com>
2022-07-23 22:50:43 -0500

    updated remix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,29 +1,34 @@
-import type { LinksFunction, LoaderFunction } from '@remix-run/node';
+import type { LinksFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-
 import { db } from '~/utils/db.server';
-
 import { useLoaderData } from '@remix-run/react';
 import ProductCard from '~/components/productCard';
-
 import indexStyles from '~/styles/index.css';
+
 export const links: LinksFunction = () => {
     return [{ rel: 'stylesheet', href: indexStyles }];
 };
 
-type LoaderData = {
-    productList: Array<{ id: string; name: string; price: number; image: string }>;
+type Product = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
 };
 
-export const loader: LoaderFunction = async () => {
+type LoaderData = {
+    productList: Array<Product>;
+};
+
+export async function loader() {
     const data: LoaderData = {
         productList: await db.product.findMany(),
     };
     return json(data);
-};
+}
 
 export default function Index() {
-    const { productList } = useLoaderData<LoaderData>();
+    const { productList } = useLoaderData<typeof loader>();
 
     return (
         <div>
