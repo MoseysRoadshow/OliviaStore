fc1a00d17aee143c9aef1610ddc31e0d05b090d2
William Jones <37749088+wdj82@users.noreply.github.com>
2022-07-26 21:59:06 -0500

    initial product page styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { db } from '~/utils/db.server';
+import { Link } from '@remix-run/react';
 import ProductCard from '~/components/productCard';
 import indexStyles from '~/styles/index.css';
 
@@ -19,9 +20,14 @@ export default function Index() {
     const { productList } = useLoaderData<typeof loader>();
 
     return (
-        <div>
-            <nav>Nav</nav>
-            <header>Olivia's Unnamed Store</header>
+        <div className='wrapper'>
+            <header>
+                <div>Menu</div>
+                <Link to='/' className='link'>
+                    Store Name
+                </Link>
+                <div>Cart</div>
+            </header>
             <main className='product-grid'>
                 {productList.map((product) => (
                     <ProductCard
--- app/routes/product/$product.tsx
+++ app/routes/product/$product.tsx
@@ -1,7 +1,8 @@
 import type { LinksFunction, LoaderArgs } from '@remix-run/node';
+import { Link } from '@remix-run/react';
 import { useLoaderData } from '@remix-run/react';
 import { db } from '~/utils/db.server';
-import productStyles from './product.css';
+import productStyles from '~/styles/product.css';
 
 export const links: LinksFunction = () => {
     return [{ rel: 'stylesheet', href: productStyles }];
@@ -27,10 +28,42 @@ export default function ProductRoute() {
     }
 
     return (
-        <div>
-            <div>{product.name}</div>
-            <div>${product.price}</div>
-            <div>{product.description}</div>
+        <div className='product-wrapper'>
+            <header>
+                <div>Menu</div>
+                <Link to='/' className='link'>
+                    Store Name
+                </Link>
+                <div>Cart</div>
+            </header>
+            <main>
+                <section className='pictures'>
+                    <img src={`/${product.featuredImage}`} alt={product.description} />
+                    {product.images.map((image, index) => (
+                        <img src={`/${image}`} key={index} alt={product.description} />
+                    ))}
+                </section>
+                <section className='right-side'>
+                    <h1>{product.name}</h1>
+                    <h2>{product.description}</h2>
+                    <h3>${product.price}</h3>
+                    <button>Add To Cart</button>
+                    <ul>
+                        <li>
+                            <h4>{product.details}</h4>
+                        </li>
+                        <li>
+                            <h4>{product.details}</h4>
+                        </li>
+                        <li>
+                            <h4>{product.details}</h4>
+                        </li>
+                        <li>
+                            <h4>{product.details}</h4>
+                        </li>
+                    </ul>
+                </section>
+            </main>
         </div>
     );
 }
